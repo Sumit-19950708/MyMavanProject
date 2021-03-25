6abf7932ce8d02ca131e4687d0f71ab366bd9ce1
Sumit <sumirshrirao@gmail.com>
2021-03-25 13:15:21 +0530

    newdir

---
+++ Sample.java
@@ -0,0 +1,11 @@
+package myjavaproject1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is CICD Class"); 
+
+	}
+
+}
